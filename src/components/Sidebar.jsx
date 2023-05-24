db05e0a06a0c38c06eae9240c069f966798820c0
Humberto <humberto.hechert2@hotmail.com>
2023-05-23 21:28:22 -0300

    finalizando component sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,17 @@
 import Avatar from '../img/humberto.jpg'
+import SocialNetworks from './SocialNetworks'
+import InformationContainer from './InformationContainer'
 import '../styles/components/sidebar.sass'
+
+
 
 const Sidebar = () => {
     return (
     <aside id="sidebar">
         <img src={Avatar} alt="Humberto Hechert" />
         <p className="title">Backend Developer</p>
-        <p>redes sociais</p>
-        <p>informações de contato</p>
+        <SocialNetworks />
+        <InformationContainer />
         <a href="" className="btn">Download Currículo</a>
     </aside>
     )
